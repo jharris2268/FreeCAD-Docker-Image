d5b301b0145fb0c5b0eca5a89851de40ff8d0494
James Harris <jimbo.harris@gmail.com>
2025-12-29 20:19:59 +0000

    added script to run docker image, commit any changes (i.e. installed workbenches or changes to settings), close image

---
+++ run_script.sh
@@ -0,0 +1,12 @@
+docker run -it -v $HOME/build/freecad_source:/mnt/source\
+    -v $HOME/build/freecad_build:/mnt/build\
+    -v $HOME/freecad_files/:/home/user/files\
+    --device /dev/dri --group-add video -e "DISPLAY=$DISPLAY" -e QT_X11_NO_MITSHM=1\
+    -v /tmp/.X11-unix:/tmp/.X11-unix:ro\
+    --user user\
+    jharris2268/freecad_docker:latest FreeCAD
+    
+export CONTAINER_ID=$(docker ps -lq)
+docker commit $CONTAINER_ID
+docker rm $CONTAINER_ID
+
